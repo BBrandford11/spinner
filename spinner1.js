9ce62842391636b913ae39e4d7c8a22e08e93fb4
BBrandford11 <brandfordbryce@gmail.com>
2022-02-15 15:40:04 -0700

    spinner

---
+++ spinner1.js
@@ -0,0 +1,36 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() =>{
+  process.stdout.write('\r|   ')
+}, 100)
+
+setTimeout(()=> {
+  process.stdout.write('\r/   ')
+},500)
+
+setTimeout(() => {
+  process.stdout.write('\r-   ')
+},1000)
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ')
+}, 1500)
+setTimeout(()=> {
+  process.stdout.write('\r|   ')
+},2000)
+
+setTimeout(() => {
+  process.stdout.write('\r/   ')
+},2500)
+
+setTimeout(() => {
+  process.stdout.write('\r-   ')
+}, 3000)
+setTimeout(()=> {
+  process.stdout.write('\r\\   ')
+},3500)
+
+setTimeout(() => {
+  process.stdout.write('\r|   ')
+},4000)
+
